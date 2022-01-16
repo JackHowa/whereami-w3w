cfa5c1e023980cd8d0ce8d08c578c863ce2290ec
Jack Howard <jackhowa@fastmail.com>
2022-01-16 16:58:15 +0000

    fix: Add error handling. Add restricted env for now until builder full concept

--- _includes/util.js
+++ _includes/util.js
@@ -1,14 +1,20 @@
 async function findWhat3Words(position) {
   try {
     const threeWordLocationResponse = await fetch(
-      `https://api.what3words.com/v3/convert-to-3wa?coordinates=${position.coords.latitude}%2C${position.coords.longitude}&key={'YOUR_API_KEY'}`
+      `https://api.what3words.com/v3/convert-to-3wa?coordinates=${position.coords.latitude}%2C${position.coords.longitude}&key={'2YIQ30MS'}`
     );
 
     const data = await threeWordLocationResponse.json();
-		document.getElementById('location-output').innerHTML = data.words;
-		// return data.words;
+
+		const threeWordLocation = data.words;
+
+		if (threeWordLocation !== undefined) {
+			document.getElementById('location-output').innerHTML = data.words;
+		} else {
+			throw new Error('No 3 word location found');
+		}
   } catch (err) {
-    // console.log(err);
+		document.getElementById('location-output').innerHTML = 'Something went wrong';
   }
 }
 
